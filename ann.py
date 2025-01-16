a2bb1fc08daacbc0aba51a1d0b5f67a1b32943f2
ant-analytics <dongvietanh@gmail.com>
2025-01-16 22:01:34 +1000

    Remove empty JSON files and obsolete README content from loan default prediction logs

--- ann.py
+++ ann.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 import keras_tuner
 from keras.utils import plot_model
 import matplotlib.pyplot as plt
+import matplotlib.style
+matplotlib.style.use('seaborn-v0_8-whitegrid') # Set global theme style
 from src.data_loader import load_data
 from src.data_preprocessing import split_data, preprocess_data, transform_features
 from src.model_train import *
@@ -31,6 +33,46 @@ outlier_detector = IsolationForest(contamination=0.007, random_state=random_stat
 X_train['outliers'] = outlier_detector.fit_predict(X_train[outlier_features])
 indices_to_remove = X_train[X_train['outliers'] == -1].index
 
+
+
+# Plot box plot
+fig, axs = plt.subplots(3, 1, figsize=(12, 12), sharex=True)
+for i, feature in enumerate(X_train[['person_age', 'person_emp_exp', 'cb_person_cred_hist_length']].columns):
+    description = metadata.loc[metadata['Feature'] == feature, 'Description'].values[0]
+    ax = axs[i]
+    boxplot = ax.boxplot([X_train[X_train['outliers'] == 1][feature], X_train[X_train['outliers'] == -1][feature]], 
+                         labels=['Inliers', 'Outliers'], vert=False)
+    ax.set_title(f'Box Plot of {description}', fontweight='bold', fontsize=14)
+    ax.set_xlabel(description)
+    ax.set_ylabel(f'Outliers Detection')
+    
+    # # Add mean annotation for inliers
+    # mean_inliers = boxplot['means'][0].get_xdata()[0]
+    # ax.annotate(f'Mean: {mean_inliers:.2f}', xy=(mean_inliers, 1), xytext=(mean_inliers + 0.5, 1.1),
+    #             horizontalalignment='center', verticalalignment='center')
+    
+    # # Add mean annotation for outliers
+    # mean_outliers = boxplot['means'][1].get_xdata()[0]
+    # ax.annotate(f'Mean: {mean_outliers:.2f}', xy=(mean_outliers, 2), xytext=(mean_outliers + 1, 2.1),
+    #             horizontalalignment='center', verticalalignment='center')
+    
+    # Draw vertical line through min values
+    # min_inliers = boxplot['whiskers'][0].get_xdata()[0]
+    min_outliers = boxplot['caps'][2].get_xdata()[0]
+    # ax.axvline(min_inliers, color='r', linestyle='--', label='Min Inliers')
+    ax.axvline(min_outliers, color='r', linestyle='--', label='Min Outliers')
+    
+    # Add tick label for red line
+    # ax.annotate(f'{min_inliers:.2f}', xy=(min_inliers, 0.5), xytext=(min_inliers, 0.5),
+    #             color='red', horizontalalignment='center', verticalalignment='center')
+    # ax.annotate(f'{min_outliers:.2f}', xy=(min_outliers, 1.5), xytext=(min_outliers, 1.5),
+    #             color='red', horizontalalignment='center', verticalalignment='center')
+    
+    ax.legend()
+
+fig.tight_layout()
+plt.show()
+
 # Remove outliers
 X_train.drop(index=indices_to_remove, columns='outliers', inplace=True)
 y_train_loan_status.drop(labels=indices_to_remove, inplace=True)
@@ -49,45 +91,6 @@ test_indices_to_remove = X_test[X_test['outliers'] == -1].index
 X_test.drop(index=test_indices_to_remove, columns='outliers', inplace=True)
 y_test_loan_status.drop(labels=test_indices_to_remove, inplace=True)
 y_test_score.drop(labels=test_indices_to_remove, inplace=True)
-
-
-
-# Plot box plot
-# fig, axs = plt.subplots(3, 1, figsize=(12, 12))
-# for i, feature in enumerate(X_train[['person_age', 'person_emp_exp', 'cb_person_cred_hist_length']].columns):
-#     ax = axs[i]
-#     boxplot = ax.boxplot([X_train[X_train['outliers'] == 1][feature], X_train[X_train['outliers'] == -1][feature]], 
-#                          labels=['Inliers', 'Outliers'], vert=False, showmeans=True, meanline=True)
-#     ax.set_title(f'Box Plot of {feature}', fontweight='bold', fontsize=14)
-#     ax.set_xlabel(feature)
-#     ax.set_ylabel(f'Outliers Detection')
-    
-#     # Add mean annotation for inliers
-#     mean_inliers = boxplot['means'][0].get_xdata()[0]
-#     ax.annotate(f'Mean: {mean_inliers:.2f}', xy=(mean_inliers, 1), xytext=(mean_inliers + 0.5, 1.1),
-#                 horizontalalignment='center', verticalalignment='center')
-    
-#     # Add mean annotation for outliers
-#     mean_outliers = boxplot['means'][1].get_xdata()[0]
-#     ax.annotate(f'Mean: {mean_outliers:.2f}', xy=(mean_outliers, 2), xytext=(mean_outliers + 1, 2.1),
-#                 horizontalalignment='center', verticalalignment='center')
-    
-#     # Draw vertical line through min values
-#     # min_inliers = boxplot['whiskers'][0].get_xdata()[0]
-#     min_outliers = boxplot['caps'][2].get_xdata()[0]
-#     # ax.axvline(min_inliers, color='r', linestyle='--', label='Min Inliers')
-#     ax.axvline(min_outliers, color='r', linestyle='--', label='Min Outliers')
-    
-#     # Add tick label for red line
-#     # ax.annotate(f'{min_inliers:.2f}', xy=(min_inliers, 0.5), xytext=(min_inliers, 0.5),
-#     #             color='red', horizontalalignment='center', verticalalignment='center')
-#     ax.annotate(f'{min_outliers:.2f}', xy=(min_outliers, 1.5), xytext=(min_outliers, 1.5),
-#                 color='red', horizontalalignment='center', verticalalignment='center')
-    
-#     ax.legend()
-
-# fig.tight_layout()
-# plt.show()
 
 # plt.figure(figsize=(12, 8))
 # plt.scatter(X_train['person_age'], X_train['person_emp_exp'], alpha=0.5)
@@ -121,13 +124,9 @@ X_train_transform, X_val_transform, X_test_transform, y_train_score_transform, y
 
 # keep track features for interpretate
 transform_features = transform_features(col_transformer)
-print(f"Data preprocessing time: {time.time() - start_time:.2f} seconds")
 
 #  plot features after transform
-import matplotlib.style
-fig, axs = plt.subplots(4, 3, figsize=(24, 18))
-matplotlib.style.use('seaborn-v0_8-whitegrid') # Set global theme style
-
+fig, axs = plt.subplots(4, 3, figsize=(24, 18), sharex=True)
 for i, feature in enumerate(transform_features):
     ax = axs[i//3, i%3]
     boxplot = ax.boxplot(X_test_transform[:, i], showfliers=True, vert=False, showmeans=True, meanline=True)
@@ -151,22 +150,29 @@ fig.tight_layout()
 plt.show()
 
 # Hyperparameter tuning
-log_dir = f"logs/fit{datetime.datetime.now().strftime('%Y%m%d-%H%M%S')}"
+tuner_log_dir = f"logs/tuner/tuner_{datetime.datetime.now().strftime('%Y%m%d-%H%M%S')}"
+tuner_save_check_point = f'save_tuner/checkpoint_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.keras'
 
 # Create TensorBoard callback
-tensorboard_callback = keras.callbacks.TensorBoard(
-    log_dir=log_dir,  
+tuner_tensorboard_callback = keras.callbacks.TensorBoard(
+    log_dir=tuner_log_dir,  
     histogram_freq=1,
     profile_batch='500,520'
 )
 
-early_stop_callback = keras.callbacks.EarlyStopping(
+tuner_early_stop_callback = keras.callbacks.EarlyStopping(
     monitor='val_output_status_accuracy',
     patience=3,
     mode='max'
 )
 
-callbacks = [tensorboard_callback, early_stop_callback]
+tuner_check_point = keras.callbacks.ModelCheckpoint(
+    filepath='best_tuner.keras',
+    monitor='val_output_status_accuracy',
+    save_best_only=True,
+)
+
+tuner_callbacks = [tuner_tensorboard_callback, tuner_early_stop_callback, tuner_check_point]
  
 tuner = keras_tuner.GridSearch(
     hypermodel=build_hyper_model,
@@ -174,8 +180,8 @@ tuner = keras_tuner.GridSearch(
         keras_tuner.Objective('val_output_status_accuracy', direction='max'),
         keras_tuner.Objective('val_output_score_mean_squared_error', direction='min')
     ],
-    max_trials=30,
-    directory=log_dir,
+    max_trials=50,
+    directory=tuner_log_dir,
     project_name='loan_default_prediction',
     overwrite=True
 )
@@ -183,35 +189,57 @@ tuner = keras_tuner.GridSearch(
 start_time = time.time()
 tuner.search(
     X_train_transform, y=[y_train_loan_status, y_train_score_transform],
-    validation_data=(X_val_transform, [y_val_loan_status, y_val_score_transform]), epochs=10,
-    callbacks=callbacks
+    validation_data=(X_val_transform, [y_val_loan_status, y_val_score_transform]), epochs=50,
+    callbacks=tuner_callbacks
 )
 
-# tuner.search(
-#     X_train, y=[y_train_loan_status, y_train_score],
-#     validation_data=(X_val, [y_val_loan_status, y_val_score]), epochs=10,
-#     callbacks=callbacks
-# )
-
-print(f"Hyperparameter tuning time: {time.time() - start_time:.2f} seconds")
-
-
 # Retrieve and save the top 10 best models
-save_dir = 'save_models/best'
+save_dir = 'save_tuner/top_best/'
 os.makedirs(save_dir, exist_ok=True)
-best_models = tuner.get_best_models(num_models=10)[0]
+best_models = tuner.get_best_models(num_models=10)
 for i, model in enumerate(best_models):
-    model.save(f'save_models/best/{i+1}_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.keras')
+    model.save(f'save_tuner/top_best/{i+1}_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.keras')
 
-save_worst = 'save_models/worst'
+save_worst = 'save_tuners/top_worst/'
 os.makedirs(save_worst, exist_ok=True)
 worst_models = tuner.get_best_models(num_models=-10)
-for i, model in enumerate(best_models):
-    model.save(f'save_models/worst/{i+1}_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.keras')
+for i, model in enumerate(worst_models):
+    model.save(f'save_models/top_worst/{i+1}_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.keras')
 
 # load the best model
-import keras
-best_models = keras.models.load_model('save_models/best/1_20250107-232302.keras')
+best_model = tuner.get_best_models(num_models=1)[0]
 
-worst_models = keras.models.load_model('save_models/worst/1_20250107-232519.keras')
+#  calbacks for fit model
+fit_log_dir = f"logs/fits/fit_{datetime.datetime.now().strftime('%Y%m%d-%H%M%S')}"
+fit_save_check_point = f'save_fits/checkpoint_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}.keras'
 
+# Create TensorBoard callback
+fit_tensorboard_callback = keras.callbacks.TensorBoard(
+    log_dir=fit_log_dir,  
+    histogram_freq=1,
+    profile_batch='500,520'
+)
+
+fit_early_stop_callback = keras.callbacks.EarlyStopping(
+    monitor='val_output_status_accuracy',
+    patience=3,
+    mode='max'
+)
+
+fit_check_point = keras.callbacks.ModelCheckpoint(
+    filepath=fit_save_check_point,
+    monitor='val_output_status_accuracy',
+    save_best_only=True,
+)
+
+fit_callbacks = [fit_tensorboard_callback, fit_early_stop_callback, fit_check_point]
+
+#  fit the best model
+history = best_model.fit(
+    X_train_transform, [y_train_loan_status, y_train_score_transform],
+    validation_data=(X_val_transform, [y_val_loan_status, y_val_score_transform]),
+    epochs=100, callbacks=fit_callbacks, shuffle=True
+)
+
+# model evaluation
+evaluation = best_model.evaluate(X_test_transform, [y_test_loan_status, y_test_score_transform], return_dict=True)
